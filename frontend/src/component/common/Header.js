daf7c7cc5809e59dbdee8105268800bbd5d88222
hyex <hyexkim@gmail.com>
2020-10-29 18:06:55 +0900

    Feat: apply styled-components module

--- frontend/src/component/common/Header.js
+++ frontend/src/component/common/Header.js
@@ -1,27 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Header = props => {
-  const headerStyle = {
-    width: '100%',
-    backgroundColor: 'black',
-    textAlign: 'center',
-    boxSizing: 'border-box',
-  }
-  const spanStyle = {
-    display: 'inline-block',
-    padding: '16px 0px',
-    color: 'white',
-    fontSize: 14,
-    fontWeight: 600,
-  }
+  const StyledHeader = styled.header`
+    width: 100%;
+    background-color: #000000;
+    text-align: center;
+    box-sizing: border-box;
+  `
 
+  const StyledSpan = styled.span`
+    display: inline-block;
+    padding: 16px 0px;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 600;
+  `
   return (
-    <header style={headerStyle} className="header">
-      <span style={spanStyle}>
         {/* issue icon */}
+    <StyledHeader className="header">
+      <StyledSpan>
         ISSUES
-      </span>
-    </header>
+      </StyledSpan>
+    </StyledHeader>
   )
 }
 
