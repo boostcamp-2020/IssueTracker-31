9ff372e8a992fc3ffa04e1e6be1c9ae972aff175
zunopark <enzoribery@naver.com>
2020-10-30 13:06:47 +0900

    Refactor: add HOST env

--- backend/src/controller/user/user.js
+++ backend/src/controller/user/user.js
@@ -5,13 +5,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const githubLogin = (req, res, next) => {
-  //   const data = userService.githubLogin
-  let state = rs.generate()
+const githubLogin = (req, res) => {
+  const state = rs.generate()
   const url = 'https://github.com/login/oauth/authorize?'
   const query = qs.stringify({
     client_id: process.env.CLIENT_ID,
-    redirect_uri: 'http://localhost:3000/githublogin',
+    redirect_uri: process.env.HOST + '/githublogin',
     state: state,
     scope: 'user:email',
   })
