f4e5f260a7595177a4ad6f4a1b794d0671b7672c
hyex <hyexkim@gmail.com>
2020-10-30 10:34:46 +0900

    Fix: moved styled component down

--- frontend/src/component/common/Header.js
+++ frontend/src/component/common/Header.js
@@ -3,33 +3,34 @@ import styled from 'styled-components'
 import icon from '@Public/images/headerIcon.png'
 
 const Header = props => {
-  const StyledHeader = styled.header`
-    width: 100%;
-    background-color: #000000;
-    text-align: center;
-    box-sizing: border-box;
-  `
-  const StyledImg = styled.img`
-    width: 14px;
-    height: 14px;
-    margin-right: 3px;
-    vertical-align: middle;
-  `
-  const StyledSpan = styled.span`
-    display: inline-block;
-    padding: 16px 0px;
-    color: #ffffff;
-    font-size: 14px;
-    font-weight: 600;
-  `
   return (
     <StyledHeader className="header">
-      <StyledSpan>
-        <StyledImg src={icon} />
+      <Span>
+        <Img src={icon} />
         ISSUES
-      </StyledSpan>
+      </Span>
     </StyledHeader>
   )
 }
+
+const StyledHeader = styled.header`
+  width: 100%;
+  background-color: #000000;
+  text-align: center;
+  box-sizing: border-box;
+`
+const Img = styled.img`
+  width: 14px;
+  height: 14px;
+  margin-right: 3px;
+  vertical-align: middle;
+`
+const Span = styled.span`
+  display: inline-block;
+  padding: 16px 0px;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 600;
+`
 
 export default Header
