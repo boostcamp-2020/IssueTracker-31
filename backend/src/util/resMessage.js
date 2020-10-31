840d667beb79cc52abc57673faffaf38e118a581
hyex <hyexkim@gmail.com>
2020-11-01 01:19:28 +0900

    Feat: add response message and status code constants

---
+++ backend/src/util/resMessage.js
@@ -0,0 +1,10 @@
+module.exports = {
+  NULL_VALUE: '필요한 값이 없습니다.',
+  OUT_OF_VALUE: '파라미터 값이 잘못 되었습니다.',
+
+  DB_ERROR: '데이터베이스 에러',
+
+  EMPTY_TOKEN: '헤더에 토큰이 없음',
+  EXPIRED_TOKEN: '유효기간이 지난 토큰',
+  INVALID_TOKEN: '잘못된 토큰',
+}
