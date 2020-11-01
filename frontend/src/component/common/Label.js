5e87d2685ca1beb6363a71d4cd0a2a70f9eab2d7
jongwon <wengers1004@naver.com>
2020-11-01 22:21:06 +0900

    Feat: add text color calculation as background color
    가독성을 위해 배경색이 어두우면 흰색, 밝으면 검은색으로 나타낼 수 있도록 계산하는 함수 추가

--- frontend/src/component/common/Label.js
+++ frontend/src/component/common/Label.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
+const Label = ({ name, backgroundColor }) => (
+  <LabelWrapper backgroundColor={backgroundColor}>{name}</LabelWrapper>
+)
 
-const Label = ({ name, color, backgroundColor }) => {
-  return (
-    <LabelWrapper color={color} backgroundColor={backgroundColor}>
-      {name}
-    </LabelWrapper>
-  )
+const getContrast = hexColor => {
+  if (hexColor.startsWith('#')) hexColor = hexColor.slice(1)
+  if (hexColor.length === 3)
+    hexColor = hexColor
+      .split('')
+      .map(hex => hex + hex)
+      .join('')
+  return getContrastFromRGB(...hexToRGB(hexColor)) >= 128 ? 'black' : 'white'
 }
 
+const getContrastFromRGB = (r, g, b) => (r * 299 + g * 587 + b * 114) / 1000
+
+const hexToRGB = hexColor => [
+  parseInt(hexColor.slice(0, 2), 16),
+  parseInt(hexColor.slice(2, 4), 16),
+  parseInt(hexColor.slice(4, 6), 16),
+]
+
 const LabelWrapper = styled.div`
-  ${({ backgroundColor, color = 'black' }) =>
+  ${({ backgroundColor }) =>
     `
     background-color: ${backgroundColor};
-    color: ${color};
+    color: ${getContrast(backgroundColor.slice(1))};
   `}
   padding: 0 7px;
   font-size: 12px;
