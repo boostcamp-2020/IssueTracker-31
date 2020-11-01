c1a4723349c2b2576c7ea3f2550c5e41e0365883
Junhyeong Jo <118jjh@naver.com>
2020-11-01 11:03:09 +0900

    Chore: delete console.log

--- backend/src/model/issue.js
+++ backend/src/model/issue.js
@@ -3,7 +3,6 @@ import query from './query/issue'
 
 const getIssues = async filterValues => {
   const sql = query.getIssuesQueryString(filterValues)
-  console.log(sql)
   try {
     const [rows] = await db.query(sql)
     return rows
