1d14b77cc1d2c6abf6702217a0f17914131f6486
zunopark <enzoribery@naver.com>
2020-11-02 01:35:53 +0900

    Feat: add oauth github login api

--- backend/src/controller/user/index.js
+++ backend/src/controller/user/index.js
@@ -4,5 +4,6 @@ import userController from './user'
 const router = express.Router()
 
 router.get('/github', userController.githubLogin)
+router.get('/githublogin', userController.handleGithubCallback)
 
 module.exports = router
--- backend/src/controller/user/user.js
+++ backend/src/controller/user/user.js
@@ -2,6 +2,8 @@ import userService from '../../service/user'
 import qs from 'querystring'
 import rs from 'randomstring'
 import dotenv from 'dotenv'
+import axios from 'axios'
+import jwt from 'jsonwebtoken'
 
 dotenv.config()
 
@@ -10,14 +12,58 @@ const githubLogin = (req, res) => {
   const url = 'https://github.com/login/oauth/authorize?'
   const query = qs.stringify({
     client_id: process.env.CLIENT_ID,
-    redirect_uri: process.env.HOST + '/githublogin',
+    redirect_uri: process.env.HOST + '/users/githublogin',
     state: state,
-    scope: 'user:email',
+    scope: 'read:user',
   })
   const githubAuthUrl = url + query
   res.send(githubAuthUrl)
 }
 
+const handleGithubCallback = (req, res) => {
+  const code = req.query.code
+  const returnedState = req.query.state
+  const githubUrl = 'https://github.com/login/oauth/access_token?'
+  const query = qs.stringify({
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code: code,
+    redirect_uri: process.env.HOST + '/users/githublogin',
+    state: returnedState,
+  })
+  const authUrl = githubUrl + query
+
+  axios
+    .post(authUrl)
+    .then(result => {
+      const config = {
+        headers: {
+          Authorization: 'token ' + qs.parse(result.data).access_token,
+          'User-Agent': 'Login-App',
+        },
+      }
+      axios
+        .get('https://api.github.com/user', config)
+        .then(githubData => {
+          const jwtToken = jwt.sign(
+            {
+              nickname: githubData.login,
+              email: githubData.email,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: '1h',
+            },
+          )
+          res.cookie('user', jwtToken, { maxAge: 3000 * 1000 })
+          res.redirect('http://localhost:4000')
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+}
+
 export default {
   githubLogin,
+  handleGithubCallback,
 }
