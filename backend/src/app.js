94a312a3d9d25ea7a78934269d40af47dde09d03
zunopark <enzoribery@naver.com>
2020-11-02 01:33:42 +0900

    Feat: remove express-session, add jsonwebtoken for auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,9 @@ import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 import Controller from './controller'
 
