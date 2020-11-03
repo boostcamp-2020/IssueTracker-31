b58eba2203e8ecaf23cd3a59be99087a95537389
hyex <hyexkim@gmail.com>
2020-11-03 14:14:02 +0900

    Feat: add context API in issue list page

--- frontend/src/page/IssueList.js
+++ frontend/src/page/IssueList.js
@@ -3,11 +3,20 @@ import SearchBar from '@Component/IssueListPage/SearchBar'
 import IssueFilter from '@Component/IssueListPage/IssueFilter'
 
 const IssueListPage = props => {
+  const [users, setUsers] = useState([])
+  const [labels, setLabels] = useState([])
+  const [milestones, setMilestones] = useState([])
+
+  useEffect(async () => {
+    const response = await getLabels()
+    setLabels(response)
+  }, [])
+
   return (
-    <>
+    <issueListContext.Provider value={{ users, labels, milestones }}>
       <SearchBar />
       <IssueFilter />
-    </>
+    </issueListContext.Provider>
   )
 }
 
