e12f205090b8b2194f118126c961fdc033cea52a
hyex <hyexkim@gmail.com>
2020-11-03 14:21:30 +0900

    Feat: add milestone request

---
+++ frontend/src/api/milestone.js
@@ -0,0 +1,11 @@
+import request from '@Util/request'
+
+const getMilestones = async () => {
+  const { success, data, message } = await request.GET(
+    'http://127.0.0.1:3000/milestones',
+  )
+  if (success === false) return console.error(message)
+  return data.data
+}
+
+export { getMilestones }
