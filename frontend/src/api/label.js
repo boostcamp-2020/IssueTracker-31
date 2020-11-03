505be2759ccb9b321c966918ba8d49f62d662e8f
hyex <hyexkim@gmail.com>
2020-11-03 14:23:17 +0900

    Chore: code clean up

--- frontend/src/api/label.js
+++ frontend/src/api/label.js
@@ -4,7 +4,6 @@ const getLabels = async () => {
   const { success, data, message } = await request.GET(
     'http://127.0.0.1:3000/labels',
   )
-  console.log(success, data)
   if (success === false) return console.error(message)
   return data.data
 }
