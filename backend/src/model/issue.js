742d9769118fc5fec827fa50b12a0dc6b6a099b6
Junhyeong Jo <118jjh@naver.com>
2020-11-05 01:10:12 +0900

    Feat: add issue create and relation make model

--- backend/src/model/issue.js
+++ backend/src/model/issue.js
@@ -11,6 +11,40 @@ const getIssues = async filterValues => {
   }
 }
 
+const postIssue = async (connection, { title, userId, milestoneId }) => {
+  try {
+    const [result] = await connection.query('INSERT INTO Issue SET ?', {
+      title,
+      userId,
+      milestoneId,
+    })
+    return result.insertId
+  } catch (err) {
+    throw new Error('DB')
+  }
+}
+
+const setRelations = async (
+  connection,
+  relations,
+  firstColumn,
+  secondColumn,
+) => {
+  try {
+    //todo : bulk insert
+    relations.forEach(async ({ table, firstId, secondId }) => {
+      const params = {}
+      params[firstColumn] = firstId
+      params[secondColumn] = secondId
+      await connection.query(`INSERT INTO ${table} SET ?`, params)
+    })
+  } catch (err) {
+    throw new Error('DB')
+  }
+}
+
 export default {
   getIssues,
+  postIssue,
+  setRelations,
 }
