59dccc1aca48be28f724603e2fd8552fa4b0ec5a
batboy118 <118jjh@naver.com>
2020-11-04 17:13:57 +0900

    Feat: capitalize table name in query

--- backend/src/model/milestone.js
+++ backend/src/model/milestone.js
@@ -1,18 +1,20 @@
 import db from './index'
 
 const getMilestone = async () => {
-  const sql = 'SELECT id, title from milestone'
+  const sql = 'SELECT id, title, dueDate, description, isOpen from Milestone'
   const [rows] = await db.query(sql)
   return rows
 }
 
-const getMilestoneDetail = async () => {
-  const sql = 'SELECT id, title, dueDate, description, isOpen from milestone'
-  const [rows] = await db.query(sql)
-  return rows
-}
+/* deprecated */
+/*
+  const getMilestoneDetail = async () => {
+    const sql = 'SELECT id, title, dueDate, description, isOpen from Milestone'
+    const [rows] = await db.query(sql)
+    return rows
+  }
+*/
 
 export default {
   getMilestone,
-  getMilestoneDetail,
 }
