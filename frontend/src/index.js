2822828cf2fea844f8afaf5defb1cc393ab63fb0
hyex <hyexkim@gmail.com>
2020-11-04 15:50:24 +0900

    Feat: make /labels router in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import IssueListPage from '@Page/IssueList'
 import Login from '@Page/Login'
+import LabelPage from '@Page/Label'
 import Header from './component/common/Header'
 const root = document.getElementById('root')
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Route exact path="/login" component={Login} />
       <Route exact path="/" component={IssueListPage} />
+      <Route exact path="/labels" component={LabelPage} />
     </BrowserRouter>
   )
 }
