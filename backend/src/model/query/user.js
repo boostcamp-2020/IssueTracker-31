cf7227e45604b62431c7d7417fd5c37d5c0d559d
zunopark <enzoribery@naver.com>
2020-11-04 17:28:04 +0900

    Feat: add user find and user store query

--- backend/src/model/query/user.js
+++ backend/src/model/query/user.js
@@ -1,5 +1,10 @@
 const getUsersQueryString = 'SELECT id, nickname, profileUrl FROM User'
-
+const findUserQueryString =
+  'SELECT id, nickname, email FROM User WHERE nickname = ?'
+const storeUserQueryString =
+  'INSERT INTO User(email, nickname, profileUrl, githubId) VALUES(?,?,?,?)'
 export default {
   getUsersQueryString,
+  findUserQueryString,
+  storeUserQueryString,
 }
