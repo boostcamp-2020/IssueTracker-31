a40da0a345ddd1a419e9d910b6194d995417febe
batboy118 <118jjh@naver.com>
2020-11-04 14:55:50 +0900

    Feat: make Button component reusable
    - Button 컴포넌트 재사용성 고려하여 button 프로퍼티 선택하는 함수 생성
    - svg, count의 유무에 따라 필요한 것만 렌더링

--- frontend/src/component/common/Button.js
+++ frontend/src/component/common/Button.js
@@ -2,32 +2,115 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-const Button = ({ buttonName, targetLocation, backgroundColor, color }) => {
+const Button = ({ name, selected }) => {
+  const { style, to, count, symbol } = getButtonProps(name, selected);
   return (
-    <StyledLink
-      to={targetLocation}
-      backgroundColor={backgroundColor}
-      color={color}
-    >
-      <span>{buttonName}</span>
+    <StyledLink to={to} styleProp={style}>
+      {symbol ? <Symbol svg={symbol} /> : ""}
+      <span>{name}</span>
+      {count ? <StyledSpan>{count}</StyledSpan> : ""}
     </StyledLink>
-  )
-}
+  );
+};
+
+const Symbol = ({ svg }) => (
+  <StyledSvg viewBox="0 0 16 16" width="16" height="16">
+    <path fill-rule="evenodd" d={svg}></path>
+  </StyledSvg>
+);
 
 const StyledLink = styled(Link)`
-  ${({ backgroundColor = '#2ea44f', color = '#ffffff' }) =>
-    `
-    color: ${color};
-    background: ${backgroundColor};
+  ${({ styleProp }) => `
+    color: ${styleProp.color};
+    background: ${styleProp.backgroundColor};
     text-decoration : none;
     cursor: pointer;
+    border: 1px solid #e1e4e8;
+    border-radius: ${styleProp.borderRadius};
+    position: relative;
+    float: left;
     padding: 5px 16px;
-    border: 1px solid rgba(27,31,35,0.15);
-    border-radius: 6px;
-    font-size: 14px;
     font-weight: 500;
     line-height: 20px;
-`}
-`
+    box-sizing: border-box;
+    white-space: nowrap!important;
+    ${
+      !styleProp.selected
+        ? `&:hover {
+      background-color: ${styleProp.hoverColor};`
+        : ""
+    }
+    `}
+`;
+
+const StyledSpan = styled.span`
+  min-width: 20px;
+  padding: 0 6px;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 18px;
+  color: #24292e;
+  text-align: center;
+  background-color: #ededed;
+  border: 1px solid transparent;
+  border-radius: 2em;
+  display: inline !important;
+  box-sizing: border-box;
+  margin-left: 4px;
+`;
+
+const StyledSvg = styled.svg`
+  box-sizing: border-box;
+  color: #24292e;
+  cursor: pointer;
+  transform-origin: 0px 0px;
+  vertical-align: text-bottom;
+  overflow: hidden;
+  margin-right: 7px;
+`;
+
+const getButtonProps = (name, selected) => {
+
+  const props = {
+    style: {
+      color: "#ffffff",
+      backgroundColor: "#2ea44f",
+      borderRadius: "6px",
+      selected
+    }
+  };
+
+  switch (name) {
+    case "Labels":
+      props.style.color = "#24292e";
+      props.style.borderRadius = "6px 0px 0px 6px";
+      props.style.backgroundColor = "#ffffff";
+      props.style.hoverColor = "#e1e4e8";
+      props.name = "Labels";
+      props.to = "/labels";
+      props.count = "1";
+      props.symbol =
+        "M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z";
+      break;
+
+    case "Milestones":
+      props.style.color = "#24292e";
+      props.style.borderRadius = "0px 6px 6px 0px";
+      props.style.backgroundColor = "#ffffff";
+      props.style.hoverColor = "#e1e4e8";
+      props.name = "Milestones";
+      props.to = "/milestones";
+      props.count = "2";
+      props.symbol =
+        "M 7.75 0 a 0.75 0.75 0 0 1 0.75 0.75 V 3 h 3.634 c 0.414 0 0.814 0.147 1.13 0.414 l 2.07 1.75 a 1.75 1.75 0 0 1 0 2.672 l -2.07 1.75 a 1.75 1.75 0 0 1 -1.13 0.414 H 8.5 v 5.25 a 0.75 0.75 0 1 1 -1.5 0 V 10 H 2.75 A 1.75 1.75 0 0 1 1 8.25 v -3.5 C 1 3.784 1.784 3 2.75 3 H 7 V 0.75 A 0.75 0.75 0 0 1 7.75 0 Z m 0 8.5 h 4.384 a 0.25 0.25 0 0 0 0.161 -0.06 l 2.07 -1.75 a 0.25 0.25 0 0 0 0 -0.38 l -2.07 -1.75 a 0.25 0.25 0 0 0 -0.161 -0.06 H 2.75 a 0.25 0.25 0 0 0 -0.25 0.25 v 3.5 c 0 0.138 0.112 0.25 0.25 0.25 h 5 Z";
+      break;
+
+    default:
+      break;
+  }
+
+  return props;
+
+};
 
 export default Button
