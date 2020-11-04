5ab17de32c40c25c5e2a4b40beada7cb8174c88c
zunopark <enzoribery@naver.com>
2020-11-04 17:46:21 +0900

    Feat: add user find and user store function

--- backend/src/service/user.js
+++ backend/src/service/user.js
@@ -1,11 +1,22 @@
 import db from '../model/user'
 
 const getUsers = async () => {
-  console.log(123)
   const users = await db.getUsers()
   return users
 }
 
+const findUser = async nickname => {
+  const user = await db.findUser(nickname)
+  return user
+}
+
+const storeUser = async data => {
+  const userId = await db.storeUser(data)
+  return userId
+}
+
 export default {
   getUsers,
+  findUser,
+  storeUser,
 }
