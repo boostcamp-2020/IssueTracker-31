b5ad1d65389d5d2f50c4a47a0b00ab09ec6e87cb
hyex <hyexkim@gmail.com>
2020-11-04 15:50:56 +0900

    Feat: add files of Label Page components

---
+++ frontend/src/component/LabelPage/TabButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TabButton = () => {
+  return <div>TabButton</div>
+}
+
+export default TabButton
--- frontend/src/page/Label.js
+++ frontend/src/page/Label.js
@@ -1,11 +1,20 @@
 import React, { createContext, useState } from 'react'
+import TabButton from '@Component/LabelPage/TabButton'
+import NewLabelButton from '@Component/LabelPage/NewLabelButton'
+import LabelList from '@Component/LabelPage/LabelList'
 
 export const labelContext = createContext()
 
 const LabelPage = props => {
   const [labels, setLabels] = useState([])
 
-  return <labelContext.Provider value={{ labels }}></labelContext.Provider>
+  return (
+    <labelContext.Provider value={{ labels }}>
+      <TabButton></TabButton>
+      <NewLabelButton></NewLabelButton>
+      <LabelList></LabelList>
+    </labelContext.Provider>
+  )
 }
 
 export default LabelPage
