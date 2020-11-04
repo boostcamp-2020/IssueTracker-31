853612f48484c55971cfd1e6a4a64e91f10adc38
Junhyeong Jo <118jjh@naver.com>
2020-11-05 01:12:29 +0900

    Feat: add service that creat isssue

--- backend/src/service/issue.js
+++ backend/src/service/issue.js
@@ -1,4 +1,51 @@
 import db from '../model/issue'
+import commnetModel from '../model/comment'
+import pool from '../model/index'
+
+const getIssues = async filterValues => {
+  if (filterValuesCheck(filterValues) === false) {
+    throw new Error('parameter')
+  }
+  const issues = await db.getIssues(filterValues)
+  return structurizeIssueList(issues)
+}
+
+const postIssue = async newIssueData => {
+  const { labeId, assignee, imageUrlId, userId, content } = newIssueData
+  // value validation 추가하기. Array.isArray(array) &&
+  // 트랜잭션 추가하기
+  const connection = await pool.getConnection()
+  const issueId = await db.postIssue(connection, newIssueData)
+  const commentId = await commnetModel.postComment(
+    issueId,
+    userId,
+    content,
+    true,
+  )
+  const issueRelationMaker = issueRelationClosure(
+    db.setRelations,
+    connection,
+    issueId,
+  )
+  //todo : bulk insert
+  await issueRelationMaker('Issue_label', 'labeId', labeId)
+  await issueRelationMaker('Issue_assignee', 'userId', assignee)
+  // 이미지 테이블에 코멘트id 넣기  //todo : bulk insert
+}
+
+//todo : bulk insert
+const issueRelationClosure = async (callback, connection, issueId) => {
+  return async (table, columnName, array) => {
+    if (array.length > 0) {
+      const relations = array.map(id => ({
+        table,
+        firstId: issueId,
+        secondId: id,
+      }))
+      await callback(connection, relations, 'issueId', columnName)
+    }
+  }
+}
 
 const structurizeIssueList = issues => {
   return issues.map(row => {
@@ -33,14 +80,7 @@ const filterValuesCheck = filterValues => {
   return true
 }
 
-const getIssues = async filterValues => {
-  if (filterValuesCheck(filterValues) === false) {
-    throw new Error('parameter')
-  }
-  const issues = await db.getIssues(filterValues)
-  return structurizeIssueList(issues)
-}
-
 export default {
   getIssues,
+  postIssue,
 }
