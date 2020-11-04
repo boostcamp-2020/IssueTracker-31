e8de0d3e503e3dcfb90d8d6f221d25a09500234d
hyex <hyexkim@gmail.com>
2020-11-04 15:59:19 +0900

    Feat: get labels and store context

--- frontend/src/page/Label.js
+++ frontend/src/page/Label.js
@@ -1,18 +1,28 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import TabButton from '@Component/LabelPage/TabButton'
 import NewLabelButton from '@Component/LabelPage/NewLabelButton'
 import LabelList from '@Component/LabelPage/LabelList'
+import { getLabels } from '@Api/label'
 
 export const labelContext = createContext()
+
+const fetchData = async (requestFn, setFn) => {
+  const response = await requestFn()
+  setFn(response)
+}
 
 const LabelPage = props => {
   const [labels, setLabels] = useState([])
 
+  useEffect(() => {
+    fetchData(getLabels, setLabels)
+  }, [])
+
   return (
-    <labelContext.Provider value={{ labels }}>
+    <labelContext.Provider value={{ labels, setLabels }}>
       <TabButton></TabButton>
       <NewLabelButton></NewLabelButton>
-      <LabelList></LabelList>
+      <LabelList>{labels}</LabelList>
     </labelContext.Provider>
   )
 }
