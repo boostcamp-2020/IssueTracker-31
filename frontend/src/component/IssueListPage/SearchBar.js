db04ec81fd64c0d59351873165d9b85235954cb7
jongwon <wengers1004@naver.com>
2020-11-05 03:12:21 +0900

    Refactor: separation of functions
    가독성을 위해 createString 함수로 분리

--- frontend/src/component/IssueListPage/SearchBar.js
+++ frontend/src/component/IssueListPage/SearchBar.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import PopUp from '../common/PopUp'
 import { issueListContext } from '@Page/IssueList'
-const SearchBar = props => {
+const SearchBar = () => {
   const { users, labels, milestones, conditions } = useContext(issueListContext)
   const [filteredString, setFilteredString] = useState('')
   const popupData = [
@@ -15,60 +15,36 @@ const SearchBar = props => {
     { id: 4, text: 'everything assinged to you' },
     { id: 5, text: 'everything mentioning to you' },
   ]
+
   useEffect(() => {
     setFilteredString(filterConditionToString(conditions))
   }, [conditions])
+
   const filterConditionToString = ({
-    author,
-    label,
-    assignee,
-    milestone,
+    author: filteredAuthor,
+    label: filteredLabel,
+    assignee: filteredAssignee,
+    milestone: filteredMilestone,
     isOpen,
   }) => {
     const result = []
-    if (author.length && users.length) {
-      result.push(
-        `author:${users.filter(user => user.id === author[0].id).name}`,
-      )
-    }
-    if (label.length && labels.length) {
-      if (label[0] === 0) {
-        result.push(`no:label`)
-      } else {
-        result.push(
-          label
-            .map(
-              id =>
-                `label:${labels
-                  .filter(target => target.id === id)
-                  .map(label => label.name)
-                  .join(' ')}`,
-            )
-            .join(' '),
-        )
-      }
-    }
-    if (assignee.length && users.length) {
-      if (assignee[0] === 0) result.push(`no:assignee`)
-      else
-        result.push(
-          assignee
-            .map(
-              id =>
-                `assignee:${users.filter(target => target.id === id).nickname}`,
-            )
-            .join(''),
-        )
-    }
-    if (milestone.length && milestones.length)
-      if (milestone[0] === 0) result.push(`no:milestone`)
-      else {
-        const [item] = milestones.filter(item => item.id === milestone[0])
-        result.push(`milestone:${item.title}`)
-      }
+    result.push(createString(users, filteredAuthor, 'nickname', 'author'))
+    result.push(createString(users, filteredAssignee, 'nickname', 'assignee'))
+    result.push(
+      createString(milestones, filteredMilestone, 'title', 'milestone'),
+    )
+    result.push(createString(labels, filteredLabel, 'name', 'label'))
     if (isOpen) result.push(`is:open`)
     else result.push(`is:close`)
     return result.join(' ')
+  }
+  const createString = (store, selectedCondition, info, condition) => {
+    if (selectedCondition.includes(0)) return `no:${condition}`
+    else
+      return store
+        .filter(target => selectedCondition.includes(target.id))
+        .map(filtered => `${condition}:${filtered[info]}`)
+        .join(' ')
   }
   return (
     <StyledSeacrchBarContainer>
@@ -81,9 +57,7 @@ const SearchBar = props => {
           <PopUp title="Filter issues" kind="text" data={popupData}></PopUp>
         </StyledDetailsMenu>
       </details>
-      <input type="text" readOnly value={filteredString}>
-        {}
-      </input>
+      <input type="text" readOnly value={filteredString} />
     </StyledSeacrchBarContainer>
   )
 }
