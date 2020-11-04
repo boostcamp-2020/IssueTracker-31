23850cc018dfbdbe3987d1fd74cab794995ac735
hyex <hyexkim@gmail.com>
2020-11-04 18:53:32 +0900

    Chore: code clean up and some remark

--- frontend/src/page/Label/Label.js
+++ frontend/src/page/Label/Label.js
@@ -6,9 +6,9 @@ import { getLabels } from '@Api/label'
 
 export const labelContext = createContext()
 
+/* TODO: 위치 변경 */
 const fetchData = async (requestFn, setFn) => {
   const response = await requestFn()
-  console.log(response)
   setFn(response)
 }
 
@@ -21,6 +21,7 @@ const LabelPage = () => {
 
   return (
     <labelContext.Provider value={{ labels, setLabels }}>
+      {/* TODO-DELETE: labels 요청 테스트를 위한 임시 코드 */}
       {labels.map(label => (
         <li key={label.id}>
           {label.name}, {label.description}
--- frontend/src/page/Label/Label.test.js
+++ frontend/src/page/Label/Label.test.js
@@ -4,6 +4,7 @@ import LabelPage from './Label'
 
 test('render Label Page', async () => {
   const { container } = render(<LabelPage />)
+  /* TODO-DELETE: labels 요청 테스트 코드. labelpage는 이 데이터를 직접 쓰지 않아서 직접 쓰는 곳으로 옮겨야 함 */
   await waitFor(() => screen.getByText('second', { exact: false }))
   expect(container).toHaveTextContent('second')
   expect(container).toBeInTheDocument()
