b4e4c796c533e86c2427cf94bd3b7b54f98322e1
batboy118 <118jjh@naver.com>
2020-11-04 14:58:42 +0900

    Feat: add LinkButton component
    - Button 컴포넌트를 재활용 하여 LinkButton 컴포넌트 생성

---
+++ frontend/src/component/common/LinkButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from 'styled-components'
+import Button from './Button'
+
+const LinkButton = ({page}) => {
+  return (
+    <StyledNav>
+      <Button name={"Labels"} selected={page === "labels"} />
+      <Button name={"Milestones"} selected={page === "milestones"} />
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  box-sizing: border-box;
+  display: flex !important;
+  white-space: nowrap !important;
+  float: left;
+  box-sizing: border-box;
+  font-size: 14px;
+  line-height: 1.5;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
+    sans-serif, Apple Color Emoji, Segoe UI Emoji;
+`;
+
+export default LinkButton
