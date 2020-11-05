1cddfde2449107f12f3153c4abceaccb63a7f249
Junhyeong Jo <118jjh@naver.com>
2020-11-05 17:05:31 +0900

    Feat: add /apt to API path

--- frontend/src/util/request.js
+++ frontend/src/util/request.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://115.85.181.174:3000'
-    : 'http://127.0.0.1:3000'
+    ? 'http://115.85.181.174:3000/api'
+    : 'http://127.0.0.1:3000/api'
 
 const options = {
   withCredentials: true,
