a8c20a3a6e7fc8025a7c73607d914344d466c480
zunopark <enzoribery@naver.com>
2020-11-05 19:05:42 +0900

    Fix: add options to cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,12 @@ const app = express()
 const port = process.env.PORT || 3000
 app.set('port', port)
 
-app.use(cors())
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  }),
+)
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
