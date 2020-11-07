9fa79864b4b86062e2bce4c6a604e200bf6c864f
Junhyeong Jo <118jjh@naver.com>
2020-11-07 22:42:21 +0900

    Feat: add label post model

--- backend/src/model/label.js
+++ backend/src/model/label.js
@@ -12,6 +12,25 @@ const getLabelByName = async name => {
   const [rows] = await db.query(sql, [name])
   return rows[0]
 }
+
+const postLabel = async ({ name, description, color }, connection) => {
+  connection = connection ? connection : db
+  const sql = query.postLabelQueryString
+  try {
+    const [result] = await connection.query(sql, {
+      name,
+      description,
+      color,
+    })
+    return result.insertId
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') throw new Error('DUPLICATE')
+    throw new Error('DB')
+  }
+}
+
 export default {
   getLabel,
+  postLabel,
+  getLabelByName,
 }
