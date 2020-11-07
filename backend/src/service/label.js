2d9278fedb07ab27af13f1e4427fdfef4a1b177f
Junhyeong Jo <118jjh@naver.com>
2020-11-07 22:42:57 +0900

    Feat: add checking new label data

--- backend/src/service/label.js
+++ backend/src/service/label.js
@@ -20,6 +20,25 @@ const getLabel = async () => {
   }
 }
 
+
+const isValidNewLabelData = ({ name, description, color }) => {
+  if (!name || !color) return false
+  if (typeof name !== 'string') return false
+  if (typeof color !== 'string' && color[0] !== '#') return false
+  if (description && typeof description !== 'string') return false
+  const rgb = color.substr(1)
+  if (rgb.length !== 6) return false
+  for (const digit of rgb) {
+    if (
+      (digit < '0' || digit > '9') &&
+      (digit < 'a' || digit > 'f') &&
+      (digit < 'A' || digit > 'F')
+    )
+      return false
+  }
+  return true
+}
+
 export default {
   getLabel,
 }
