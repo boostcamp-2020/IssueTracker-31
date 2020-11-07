f6739b34e4e17f5c51a843c080713a32a63045bc
jongwon <wengers1004@naver.com>
2020-11-07 22:00:37 +0900

    Feat: add get milestone detail api

--- backend/src/controller/milestone/index.js
+++ backend/src/controller/milestone/index.js
@@ -4,5 +4,5 @@ import milestoneController from './milestone'
 const router = express.Router()
 
 router.get('/', milestoneController.read)
-
+router.get('/detail', milestoneController.readDetail)
 module.exports = router
