b626cb17c28d9e8213b673218b787d7ef0fe10c3
Junhyeong Jo <118jjh@naver.com>
2020-11-07 22:41:45 +0900

    Feat: add model for label searching by name

--- backend/src/model/label.js
+++ backend/src/model/label.js
@@ -1,11 +1,17 @@
 import db from './index'
+import query from './query/label'
 
 const getLabel = async () => {
-  const sql = 'SELECT id, name, description, color from Label'
+  const sql = query.getLabelQueryString
   const [rows] = await db.query(sql)
   return rows
 }
 
+const getLabelByName = async name => {
+  const sql = query.getLabelByNameQueryString
+  const [rows] = await db.query(sql, [name])
+  return rows[0]
+}
 export default {
   getLabel,
 }
