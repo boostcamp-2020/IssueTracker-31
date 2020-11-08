d8a1add81a2ca39e7fc68dfbec7795ebfcdd0321
batboy118 <118jjh@naver.com>
2020-11-08 14:16:09 +0900

    Feat: write query string for update label

--- backend/src/model/query/label.js
+++ backend/src/model/query/label.js
@@ -2,10 +2,12 @@ const getLabelQueryString = 'SELECT id, name, description, color from Label'
 const postLabelQueryString = 'INSERT INTO Label SET ?'
 const getLabelByNameQueryString = 'SELECT id FROM Label WHERE name=?'
 const deleteLabelQueryString = 'DELETE FROM Label WHERE id=?'
+const patchLabelQueryString = 'UPDATE Label SET ?  WHERE id=?'
 
 export default {
   getLabelQueryString,
   postLabelQueryString,
   getLabelByNameQueryString,
   deleteLabelQueryString,
+  patchLabelQueryString,
 }
