dfa12cddf521a5b5f45e7c5653ba0822da40dea0
Junhyeong Jo <118jjh@naver.com>
2020-11-08 22:24:59 +0900

    Feat: add get milestion on issuel model

--- backend/src/model/milestone.js
+++ backend/src/model/milestone.js
@@ -1,14 +1,32 @@
 import db from './index'
 import query from './query/milestone'
+
 const getMilestone = async () => {
   const [rows] = await db.query(query.getMilestone)
   return rows
 }
+
 const getMilestoneDetail = async () => {
   const [rows] = await db.query(query.getMilestoneDetail)
   return rows
 }
+
+const getMilestoneOnIssue = async (issueId, connection) => {
+  connection = connection ? connection : db
+  try {
+    const [milestone] = await connection.query(
+      query.getMilestoneOnIssueQueryString,
+      issueId,
+    )
+    return milestone
+  } catch (err) {
+    console.log(err)
+    throw new Error('DB')
+  }
+}
+
 export default {
   getMilestone,
   getMilestoneDetail,
+  getMilestoneOnIssue,
 }
