d5f65ef4d248b95e64c179743105c420d934b9d2
kimhyeji <hyexkim@gmail.com>
2020-11-10 00:58:50 +0900

    Feature/image upload (#58, #59) (#183)
    * Feat: add image input UI
    
    * Feat: add image upload request
    
    * Feat: complete image input component
    
    * Feat: change file to fordata object

--- frontend/src/component/common/ImageInput.js
+++ frontend/src/component/common/ImageInput.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { createImageUrl } from '@Api/comment'
 
 const ImageInput = () => {
-  const textStatus = 'error'
+  const [status, setStatus] = useState('default')
 
+  const getImageUrl = async e => {
+    setStatus('loading')
+    const formData = new FormData()
+    formData.append('image', e.target.files[0])
+    const { success, url } = await createImageUrl(formData)
+    if (!success) return setStatus('error')
+    console.log(url) // TODO: 상위 컴포넌트로 넘겨서 마크다운 문법 텍스트로 변환하기
+    setStatus('default')
+  }
   return (
     <StyledWrapper>
-      <StyledInput type="file" accept=".jpeg,.png,.jpg"></StyledInput>
-      <StyledText status={textStatus === 'default'}>
+      <StyledInput
+        type="file"
+        accept=".jpeg,.png,.jpg"
+        onChange={getImageUrl}
+      ></StyledInput>
+      <StyledText status={status === 'default'}>
         Attach files by dragging & dropping, selecting or pasting them.
       </StyledText>
-      <StyledText status={textStatus === 'loading'}>
+      <StyledText status={status === 'loading'}>
         <img
           alt=""
           width="16"
@@ -19,6 +33,7 @@ const ImageInput = () => {
         />
         <StyledSpan>Loading</StyledSpan>
       </StyledText>
+      <StyledText status={status === 'error'}>Error</StyledText>
       <StyledText status={textStatus === 'error'}>Error</StyledText>
     </StyledWrapper>
   )
