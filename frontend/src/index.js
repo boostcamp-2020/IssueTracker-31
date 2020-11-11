d5f012dd41d5ee88593f077c3f3a0a669af8009c
hyex <hyexkim@gmail.com>
2020-11-11 23:44:07 +0900

    Style: set global style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 import Login from '@Page/Login'
 import IssueListPage from '@Page/IssueList'
 import LabelPage from '@Page/Label/Label'
@@ -14,16 +15,35 @@ const root = document.getElementById('root')
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/" component={IssueListPage} />
-      <Route exact path="/labels" component={LabelPage} />
-      <Route exact path="/milestones" component={MilestonePage} />
-      <Route exact path="/milestone/new" component={CreateMilestonePage} />
-      <Route exact path="/issue/new" component={CreateIssuePage} />
-      <Route exact path="/issues/:id" component={IssueDetailPage} />
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={IssueListPage} />
+        <Route exact path="/labels" component={LabelPage} />
+        <Route exact path="/milestones" component={MilestonePage} />
+        <Route exact path="/milestone/new" component={CreateMilestonePage} />
+        <Route exact path="/issue/new" component={CreateIssuePage} />
+        <Route exact path="/issues/:id" component={IssueDetailPage} />
+      </BrowserRouter>
+    </>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: -apple-system,Arial,sans-serif;
+    font-size: 14px;
+    line-height: 1.5;
+  }
+	body {
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+    width: 100%;
+	}
+`
+
 ReactDom.render(<App />, root)
