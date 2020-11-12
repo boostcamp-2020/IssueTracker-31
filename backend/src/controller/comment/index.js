9951880b3c90229cbd6f6dda4010c44c3a00a767
zunopark <enzoribery@naver.com>
2020-11-12 10:35:30 +0900

    Feat: change static file path in server

--- backend/src/controller/comment/index.js
+++ backend/src/controller/comment/index.js
@@ -4,7 +4,7 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, 'public/images/')
   },
   filename: function (req, file, cb) {
     cb(null, new Date().toISOString() + file.originalname)
